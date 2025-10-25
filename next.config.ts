a4d111ce22558dfd7b3e23b2ae2987f2a9818760
Steve Helms <steve@helms.place>
2025-10-25 13:00:09 -0500

    next config updates for DO deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: "_static",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
