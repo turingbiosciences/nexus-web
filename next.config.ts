c84898b86003724c44e60657f84ecbc2e93a2195
Steve Helms <steve@helms.place>
2025-10-26 09:11:03 -0500

    rolling back next.config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
-  distDir: "_static",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
